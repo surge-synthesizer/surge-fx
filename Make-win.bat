bb915e64a9fff12620c45b4a7c808fc100041e04
Paul W <paul@pwjw.com>
2019-07-22 19:00:56 -0400

    More win azure

--- Make-win.bat
+++ Make-win.bat
@@ -1,14 +1,14 @@
 mkdir assets
-mkdir products
+
 
 curl -o assets/juce-5.4.3-windows.zip https://d30pueezughrda.cloudfront.net/juce/juce-5.4.3-windows.zip
 cd assets && unzip juce-5.4.3-windows.zip
+cd ..
 
 assets\JUCE\Projucer.exe --resave surge-fx.jucer
 cd Builds\VisualStudio2017
 msbuild /target:Build surge-fx.sln /p:Configuration=Release;Platform=x64
-cd x64\
-7z.exe a ..\..\..\products\surge-fx-win.zip Release
+
 
 
 
