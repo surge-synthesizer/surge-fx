f08deb695a7cf3bf00eb37ad81e6851a189cde69
Paul W <paul@pwjw.com>
2019-09-01 22:39:29 -0400

    Turn on Win32
    In theory, this will turn on win32 in the pipelines.

--- Make-win.bat
+++ Make-win.bat
@@ -1,13 +1,14 @@
-mkdir assets
+rem mkdir assets
 
 
-curl -o assets/juce-5.4.3-windows.zip https://d30pueezughrda.cloudfront.net/juce/juce-5.4.3-windows.zip
-cd assets && unzip juce-5.4.3-windows.zip
-cd ..
+rem curl -o assets/juce-5.4.3-windows.zip https://d30pueezughrda.cloudfront.net/juce/juce-5.4.3-windows.zip
+rem cd assets && unzip juce-5.4.3-windows.zip
+rem cd ..
 
 assets\JUCE\Projucer.exe --resave surge-fx.jucer
 cd Builds\VisualStudio2017
 msbuild /target:Build surge-fx.sln /p:Configuration=Release;Platform=x64
+msbuild /target:Build surge-fx.sln /p:Configuration=ReleaseWin32;Platform=Win32
 
 
 
